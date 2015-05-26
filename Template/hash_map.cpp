c975615a6a228d5d3c0dfbb903a93d29e5e92b02
ck89119 <ck89119@gmail.com>
2015-05-26 20:41:11 +0800

    change hash_map.cpp

--- Template/hash_map.cpp
+++ Template/hash_map.cpp
@@ -1,10 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define dump(x) cerr <<  __LINE__ << " : "<< #x << "  =  " << (x) << endl
+#define CLR(a, x) memset(a, x, sizeof(a)) //0:0, -1:-1, INF:0x3f, -INF:0x80
+#define MP(A, B) make_pair(A, B)
+#define PB(A) push_back(A)
+#define lson l, m, rt << 1
+#define rson m + 1, r, rt << 1 | 1
+#define lowbit(x) x & (-x)
+typedef long long LL;
+typedef unsigned long long ULL;
+typedef pair<int, int> PII;
+
+template <class T> void out(T A[],int n){for (int i=0;i<n;i++) cout<<A[i]<<" ";cout<<endl;}
+template <class T> void out(vector<T> A,int n=-1){if(n==-1) n=A.size();for (int i=0;i<n;i++) cout<<A[i]<<" ";cout<<endl;}
+const int N = 999999;
+const int M = 9999999;
+const int INF = 0x3f3f3f3f;
 const int MOD = 10003;
-const int MAXN = 999999;
 const int SEED = 13331;  // 10003, 10007, 13331, 1000000007, 1000000009
 
 struct HashMap {
-  int head[MOD], next[MAXN], cnt[MAXN];
-  ULL data[MAXN];
+  struct Node {
+    long long key, value;
+    int next;
+  } data[N];
+  int head[MOD];
   int size;
 
   int init() {
@@ -13,25 +34,59 @@ struct HashMap {
     return 0;
   }
 
-  int insert(ULL key) {
+  int find(long long key) {
     int k = key % MOD;
     for (int i = head[k]; i != -1; i = next[i])
-      if (data[i] == key) {
-        ++cnt[i];
-        return 0;
-      }
-    data[size] = key;
-    next[size] = head[k];
-    cnt[size] = 1;
+      if (data[i].key == key) return i;
+    return -1;
+  }
+
+  int insert(long long key) {
+    int k = key % MOD;
+    for (int i = head[k]; i != -1; i = data[i].next)
+      if (data[i].key == key) return 0;
+    data[size].key = key;
+    data[size].value = 0;
+    data[size].next = head[k];
     head[k] = size++;
     return 0;
   }
 
-  int count(ULL key) {
-    int k = key % MOD;
-    for (int i = head[k]; i != -1; i = next[i])
-      if (data[i] == key) return cnt[i];
+  int get(long long key, long long &value) {
+    int i = find(key);
+    if (i != -1) value = data[i].value;
+    return i;
+  }
+
+  int set(long long key, long long value) {
+    int i = find(key);
+    if (i == -1) {
+      insert(key);
+      data[size-1].value = value;
+    } else {
+      data[i].value = value;
+    }
+    return 0;
+  }
+
+  int add(long long key, long long delta) {
+    int i = find(key);
+    if (i == -1) {
+      insert(key);
+      data[size-1].value += delta;
+    } else {
+      data[i].value += delta;
+    }
     return 0;
   }
 
 };
+
+int main() {
+#ifndef ONLINE_JUDGE
+  freopen("in.txt", "r", stdin);
+  //freopen("out.txt", "w", stdout);
+#endif
+  
+  return 0;
+}
