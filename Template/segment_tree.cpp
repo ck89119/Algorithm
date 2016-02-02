ccfb7f9ee5f7b61dd6e3ea811c9d00360027c9b3
ck89119 <ck89119@gmail.com>
2016-02-02 14:09:52 +0800

    modify segment_tree.cpp

--- Template/segment_tree.cpp
+++ Template/segment_tree.cpp
@@ -2,15 +2,17 @@
 using namespace std;
 
 #define dump(x) cerr <<  __LINE__ << " : "<< #x << "  =  " << (x) << endl
-#define CLR(a, x) memset(a, x, sizeof(a)) //0:0, -1:-1, INF:0x3f, -INF:0x80
-#define MP(A, B) make_pair(A, B)
-#define PB(A) push_back(A)
+#define clr(a, x) memset(a, x, sizeof(a)) //0:0, -1:-1, INF:0x3f, -INF:0x80
+#define mp(a, b) make_pair(a, b)
+#define pb(a) push_back(a)
 #define lson l, m, rt << 1
 #define rson m + 1, r, rt << 1 | 1
 #define lowbit(x) x & (-x)
-typedef long long LL;
-typedef unsigned long long ULL;
-typedef pair<int, int> PII;
+#define X first 
+#define Y second
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
 
 template <class T> void out(T A[],int n){for (int i=0;i<n;i++) cout<<A[i]<<" ";cout<<endl;}
 template <class T> void out(vector<T> A,int n=-1){if(n==-1) n=A.size();for (int i=0;i<n;i++) cout<<A[i]<<" ";cout<<endl;}
@@ -19,8 +21,21 @@ const int M = 9999999;
 const int INF = 0x3f3f3f3f;
 const int MOD = 1000000007;
 
-int t[N];
+int t[2*N], d[N];
 int n;
+int h;
+
+int push_up(int p) {
+  while (p > 1) {
+    p >>= 1;
+    t[p] = max(t[p>>1], t[p>>1|1]) + d[p];
+  }
+  return 0;
+}
+
+int push_down(int p) {
+  return 0;
+}
 
 int build() {
   for (int i = n - 1; i > 0; --i)
@@ -28,13 +43,13 @@ int build() {
   return 0;
 }
 
-int modify(int p, int v) {
+int add(int p, int v) {
   p += n;
-  for (t[p] = v; p > 1; p >>= 1) t[p>>1] = t[p] + t[p^1];
+  for (t[p] += v; p > 1; p >>= 1) t[p>>1] = t[p] + t[p^1];
   return 0;
 }
 
-int modify(int l, int r, int v) {
+int add(int l, int r, int v) {
   for (l += n, r += n; l < r; l >>= 1, r >>= 1) {
     if (l & 1) t[l++] += v;
     if (r & 1) t[--r] += v;
@@ -58,10 +73,11 @@ int main() {
   //freopen("out.txt", "w", stdout);
 #endif
   n = 10;
+  h = sizeof(int) * 8 - __builtin_clz(n);
   for (int i = 0; i < n; ++i)
     t[i+n] = i;
   build();
-  modify(0, 10);
+  add(0, 10);
   out(t, 2 * n);
   for (int i = 1; i <= n; ++i)
     cout << query(0, i) << endl;
