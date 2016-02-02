7683533a81ea0a40590dc4244681982f5a4544b9
ck89119 <ck89119@gmail.com>
2016-02-02 14:09:16 +0800

    rewrite union_find_set.cpp

--- Template/union_find_set.cpp
+++ Template/union_find_set.cpp
@@ -1,34 +1,70 @@
-const int maxn=999999;
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <fstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+#define dump(x) cerr <<  __LINE__ << " : "<< #x << "  =  " << (x) << endl
+#define clr(a, x) memset(a, x, sizeof(a)) //0:0, -1:-1, INF:0x3f, -INF:0x80
+#define mp(a, b) make_pair(a, b)
+#define pb(a) push_back(a)
+#define lson l, m, rt << 1
+#define rson m + 1, r, rt << 1 | 1
+#define lowbit(x) x & (-x)
+#define X first 
+#define Y second
+typedef long long ll;
+typedef pair<int, int> pii;
+
+template <class T> void out(T A[],int n){for (int i=0;i<n;i++) cout<<A[i]<<" ";cout<<endl;}
+template <class T> void out(vector<T> A,int n=-1){if(n==-1) n=A.size();for (int i=0;i<n;i++) cout<<A[i]<<" ";cout<<endl;}
+const int N = 100000 + 5;
+const int INF = 0x3f3f3f3f;
+const int MOD = 1000000007;
+
 class UnionFindSet {
-  public:
-    int rank[maxn], pnt[maxn];
-    int init() {
-      memset(rank, 0, sizeof(rank));
-      memset(pnt, 0, sizeof(pnt));
-      return 0;
-    }
-    UnionFindSet() { init(); }
-    int makeset(int x) {
-      rank[pnt[x] = x] = 0;
-      return 0;
-    }
-    int find(int x) { //可以写个递归更简单
-      int px = x, i;
-      while (px != pnt[px]) px = pnt[px];
-      while (x != px) {
-        i = pnt[x];
-        pnt[x] = px;
-        x = i;
-      }
-      return px;
-    }
-    int merge(int x, int y) { // or  just  pnt[find(x)] = find(y)
-      if (rank[x=find(x)] > rank[y=find(y)]) {
-        pnt[y] = x;
-      } else {
-        pnt[x] = y;
-        rank[y] += (rank[x] == rank[y]);
-      }
-      return 0;
-    }
+ public:
+  int f[N];
+  UnionFindSet() {
+    for (int i = 0; i < N; ++i)
+      f[i] = i;
+  }
+
+  int find(int x) {
+    if (x == f[x]) return x;
+    return f[x] = find(f[x]);
+  }
+
+  int merge(int x, int y) {
+    f[x = find(x)] = f[y = find(y)];
+    return 0;
+  }
+};
+
+int main() {
+#ifndef ONLINE_JUDGE
+  freopen("in.txt", "r", stdin);
+  //freopen("out.txt", "w", stdout);
+#endif
+  
+  return 0;
 }
+
