482b5db917b05b6fb92f73d68a478fbbdd0d2102
caokai <caokai.1109@bytedance.com>
2021-01-30 14:20:47 +0800

    solve google kickstart 2017 round D Problem C

--- kickstart/2017/D/C.cpp
+++ kickstart/2017/D/C.cpp
@@ -8,60 +8,88 @@ using namespace std;
 #define lowbit(x) x & (-x)
 typedef long long ll;
 typedef pair<int, int> pii;
+typedef vector<vector<double>> vvd;
 
 template <class T> void out(T A[],int n){for (int i=0;i<n;i++) cout<<A[i]<<" ";cout<<endl;}
 template <class T> void out(vector<T> A,int n=-1){if(n==-1) n=A.size();for (int i=0;i<n;i++) cout<<A[i]<<" ";cout<<endl;}
 const int N = 10 + 5;
 const int INF = 0x3f3f3f3f;
 const int MOD = 1000000007;
-const double eps = 1e-6;
+const double eps = 1e-10;
 
 int n, p, h;
 int x[N], y[N];
 
-double dis_sqr(double x0, double y0, double x1, double y1) {
-  return (x0 - x1) * (x0 - x1) + (y0 - y1) * (y0 - y1);
-}
-
-double f(double a, double x) {
-  return a * x * (x - p);
-}
-
-bool check(int radiu, int x, int y) {
-
-}
-
-bool check_all(double radiu) {AAAAAAAAAAAAAAA
-  double l = -4.0 * h / p / p;
-  double r = 1.0 * y / x / (x-p);
-  if (l >= r) return y >= radiu;
+bool outside(double a, double x, double y, double radiu) {
+  double a4 = a * a;
+  double a2 = -2 * a * y + 1;
+  double a1 = -2 * x;
+  double a0 = x * x + y * y - radiu * radiu;
+  double b3 = 4 * a4;
+  double b1 = 2 * a2;
+  double b0 = a1;
   
-  double r_sqr = radiu * radiu;
-  for (int k = 0; k < 50; ++k) {
-    // dump(l), dump(r);
-    double a = (l + r) / 2;
-    if (h + a * p * p / 4 < radiu) {
-      l = a;
-      continue;
-    }
-
-    bool flag = true;
-    for (double x0 = 0.0; x0 <= p; x0 += 0.01)
-      if (dis_sqr(x, y, x0, f(a, x0)) < r_sqr) {
-        flag = false;
-        break;
-      }
-    if (flag) return true;
-    r = a;
+  double l = -p / 2.0, r = 0; 
+  while (r - l > eps) {
+    double m = (l + r) / 2;
+    if (b3 * m * m * m + b1 * m + b0 <= 0) l = m;
+    else r = m;
   }
-  return false;
+  return a4 * l * l * l * l + a2 * l * l + a1 * l + a0 > eps;
+}
+
+void add_intersection(double l, double r, const vvd& a, vvd& b) {
+  for (auto &v: a) {
+    double ll = max(l, v[0]), rr = min(r, v[1]);
+    if (ll <= rr - eps) {
+      b.push_back({ll, rr});
+    }
+  }
+}
+
+bool check(double radiu) {
+  // dump(radiu);
+  double a_min = -4.0 * (h - radiu) / p / p;
+  vvd ans = {{a_min, 0}};
+
+  for (int i = 0; i < n; ++i) {
+    double l, r;
+    vvd cur;
+  
+    // [a_min, ...]
+    l = a_min;
+    r = 1.0 * y[i] / x[i] / (x[i] - p);
+    // dump(l), dump(r);
+    while (r - l > eps) {
+      double m = (l + r) / 2;
+      if (outside(m, x[i] - p / 2.0, y[i] + m * p * p / 4, radiu)) l = m;
+      else r = m;
+    }
+    add_intersection(a_min, l, ans, cur);
+    // for (auto p: cur) cout << p[0] << " " << p[1] << endl;
+
+    // [..., 0]
+    l = 1.0 * y[i] / x[i] / (x[i] - p);
+    r = 0;
+    while (r - l > eps) {
+      double m = (r + l) / 2;
+      if (outside(m, x[i] - p / 2.0, y[i] + m * p * p / 4, radiu)) r = m;
+      else l = m;
+    }
+    add_intersection(r, 0, ans, cur);
+    // for (auto p: cur) cout << p[0] << " " << p[1] << endl;
+
+    ans = cur;
+  }
+
+  return !ans.empty();
 } 
 
 int main() {
-#ifndef ONLINE_JUDGE
-  freopen("in.txt", "r", stdin);
-  freopen("out.txt", "w", stdout);
-#endif
+// #ifndef ONLINE_JUDGE
+//   freopen("in.txt", "r", stdin);
+//   freopen("out.txt", "w", stdout);
+// #endif
   int T;
   scanf("%d", &T);
   for (int ncase = 1; ncase <= T; ++ncase) {
@@ -69,13 +97,13 @@ int main() {
     scanf("%d%d%d", &n, &p, &h);
     for (int i = 0; i < n; ++i) {
       scanf("%d%d", &x[i], &y[i]);
+      if (2 * x[i] > p) x[i] = p - x[i];
     }
   
     double l = 0, r = h;
     while (r - l > eps) {
       double m = (r + l) / 2;
-      // dump(m);
-      if (check_all(m)) l = m;
+      if (check(m)) l = m;
       else r = m;
     }
     printf("Case #%d: %.9lf\n", ncase, l);
